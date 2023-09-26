ed9ae2b48edfac001f0cb2a772d0d9f6540bc8b7
EZP26 <ezpham1@gmail.com>
2023-09-26 13:05:09 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let trunkHeight = 75;
 let uniWidth = 5;
-let minBranchHeight = 40;
+let minBranchHeight = 20;
 
 function setup() {
   createCanvas(700, 700);
@@ -8,20 +8,21 @@ function setup() {
   background(220);
   stroke(0);
   translate(width / 2, height);
-  branch(trunkHeight, uniWidth, 10);
+  branch(trunkHeight, uniWidth, 20);
 }
 
 function branch(branchHeight, branchWidth, angle){
   if (branchHeight < minBranchHeight){
     return;
   }
+
   strokeWeight(branchWidth);
   line(0, 0, 0, - branchHeight);
   translate(0, -branchHeight);
-  angle += Math.floor(Math.random() * 30);
+  angle += Math.floor(Math.random() * 10);
   
   branchHeight = branchHeight * 0.9;
-  branchWidth = branchWidth * 0.95;
+  branchWidth = branchWidth * 0.9;
 
   push();
   rotate(angle);
